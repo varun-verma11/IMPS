a58f1f1880fad21bb912f1c6e5f54c3838f8a299
Ganesh Narendra Kumar <gn511@visual21.doc.ic.ac.uk>
2012-06-05 13:18:04 +0100

    Started writing a tokeniser on a different script. Will merge the two before submission is due.

--- src/ganesh.c
+++ src/ganesh.c
@@ -1,3 +1,12 @@
-**char () {
-
+**char (instruction[][]) {
+  /*
+  This should return an array of strings, splitting up the instructions into
+  tokens. Tokens should cover labels (the first piece of text ending in a 
+  colon ":"), operations (such as add, addi, mul, muli, etc) and operands (such
+  as the registers where data is found and stored or the immediate values to be
+  used). 
+  
+  issue: Should I further split them into operand types, ie explicitly state if 
+  it's a register, constant or address?)
+  */
 }
