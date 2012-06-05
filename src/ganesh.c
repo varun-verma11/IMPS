228b41d664b5fb33e3fab18ae478809d1dd28339
Ganesh Narendra Kumar <gn511@visual20.doc.ic.ac.uk>
2012-06-05 15:55:12 +0100

    Tokeniser needs debugging.

--- src/ganesh.c
+++ src/ganesh.c
@@ -1,4 +1,9 @@
-**char (instruction[][]) {
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+  char **tokeniser(char *instruction) {
   /*
   This should return an array of strings, splitting up the instructions into
   tokens. Tokens should cover labels (the first piece of text ending in a 
@@ -9,4 +14,40 @@
   issue: Should I further split them into operand types, ie explicitly state if 
   it's a register, constant or address?)
   */
+  
+  int i;
+  char *str1, *token;
+  char *saveptr1;
+  char **tokens;
+  char labelTokenDelim[] = ": ";
+  char opTokenDelim[] = " ";
+  
+  
+  for (i = 0, str1 = instruction; ;i++, str1 = NULL) {
+    token = strtok_r(str1, labelTokenDelim, &saveptr1);
+    if (token == NULL) break;
+    tokens[i] = token;
+    printf("%s\n", token);
+  }
+  for (str1 = instruction; ;i++, str1 = NULL) {
+    token = strtok_r(str1, opTokenDelim, &saveptr1);
+    if (token == NULL) break;
+    tokens[i] = token;
+    printf("%s\n", token);
+  }
+  return tokens;
+ 
+}
+
+int main(void) {
+  char ins[] = "Label: addi $31 $32 32";
+  char instructionPtr = *ins;
+  char **tokens = tokeniser(&instructionPtr);
+  int i;
+  for(i = 0; ;i++) {
+    while(tokens[i] != NULL) {
+      printf("%s\n", tokens[i]);
+    }
+  }
+  return 0;
 }
