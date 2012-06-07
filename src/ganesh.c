bf6228b5e8d557a0832e5d904c39c7a30eb4c43d
Varun Verma <vv311@visual21.doc.ic.ac.uk>
2012-06-07 16:02:10 +0100

    finished the method for tokenising and the ADT symbol table and tested them and they work.

--- src/ganesh.c
+++ src/ganesh.c
@@ -3,36 +3,19 @@
 #include <stdio.h>
 
 
-char **tokeniser(char *instruction) {
+void tokeniser(char instruction[], char **token) {
 
-  printf("%s \n",instruction);
-  //http://www.linuxquestions.org/questions/programming-9/question-about-strtok_r-built-in-function-in-c-181144/
-  //http://www.kirupa.com/forum/archive/index.php/t-233004.html
   char *buf_pp = NULL;
   char *s;
   int count =0;
-  char **token = malloc(sizeof(char*)*3);
-  //printf("size of tokens  ->  %d \n",(sizeof(token)));
-  printf("check 2\n");
     
   /* Parse the first argument */
-  token[count] = strtok_r(&instruction, ":", &buf_pp);
+  token[count] = strtok_r(instruction, ":", &buf_pp);
   do {
-    /* Print the current token */
-    printf ("token  -> %s \n", token[count]);
     /* Increment count */
     count++;
     /* Get the next token until end of string */
     token[count] = strtok_r(NULL, " ", &buf_pp);
-  } while (token[count-1]!=NULL);
-  /* Done */
-  printf ("Done.\n");
-  return token;
+  } while (token[count]!=NULL);
 }
 
-int main(void) {
-
-  char **tokens = tokeniser("label: addi $1 $23 32");  
-  
-  return 0;
-}
