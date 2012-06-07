a1872c9f9143c6ebca0ce6f87d951f6161279cb6
varun <varun@ubuntu.(none)>
2012-06-07 13:08:25 +0100

    wrote a new table since the preivious one had errors. Also worked on tokeniser but still the errors are there

--- src/ganesh.c
+++ src/ganesh.c
@@ -6,25 +6,25 @@
 char **tokeniser(char *instruction) {
 
   printf("%s \n",instruction);
-  
-  printf("string length     ->  %d \n",strlen(instruction));
+  //http://www.linuxquestions.org/questions/programming-9/question-about-strtok_r-built-in-function-in-c-181144/
+  //http://www.kirupa.com/forum/archive/index.php/t-233004.html
   char *buf_pp = NULL;
-  char *s = NULL;
+  char *s;
   int count =0;
   char **token = malloc(sizeof(char*)*3);
   //printf("size of tokens  ->  %d \n",(sizeof(token)));
   printf("check 2\n");
     
   /* Parse the first argument */
-  token[count] = strtok_r(instruction, ":", &buf_pp);
+  token[count] = strtok_r(&instruction, ":", &buf_pp);
   do {
     /* Print the current token */
-    printf ("token  -> %s\n", token[count]);
+    printf ("token  -> %s \n", token[count]);
     /* Increment count */
     count++;
     /* Get the next token until end of string */
     token[count] = strtok_r(NULL, " ", &buf_pp);
-  } while (token[count1-1]!=NULL);
+  } while (token[count-1]!=NULL);
   /* Done */
   printf ("Done.\n");
   return token;
