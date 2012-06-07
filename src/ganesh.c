167e90d7e9c6f5bde9d86cfa7d5cd84eb340ede6
varun <varun@ubuntu.(none)>
2012-06-07 22:46:38 +0100

    tried to fix tokeniser

--- src/ganesh.c
+++ src/ganesh.c
@@ -3,19 +3,30 @@
 #include <stdio.h>
 
 
-void tokeniser(char instruction[], char **token) {
 
+int main(void) {
+  //char **token = (char **) malloc(sizeof(char *) *5);
+  
+  static char ip_addr[] = "label: add $1 $2 $3";
   char *buf_pp = NULL;
-  char *s;
-  int count =0;
-    
-  /* Parse the first argument */
-  token[count] = strtok_r(instruction, ":", &buf_pp);
-  do {
+  char *s = NULL;
+  int count = 0;
+
+  s = strtok_r (ip_addr, ":", &buf_pp);
+
+  while (s) {
     /* Increment count */
     count++;
-    /* Get the next token until end of string */
-    token[count] = strtok_r(NULL, " ", &buf_pp);
-  } while (token[count]!=NULL);
-}
 
+    /* Print the current token */
+    printf ("token[%d]: %s\n",
+    count, s);
+
+    /* Get the next token until end of string */
+    s = strtok_r (NULL, " ", &buf_pp);
+  }
+  printf ("Done.\n");
+  
+  //tokeniser(ins); 
+  return EXIT_SUCCESS;
+}
