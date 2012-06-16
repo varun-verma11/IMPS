348cc881238e9e6e14f226c592956088eebdd64b
Jose <jose@jose-laptop.(none)>
2012-06-16 12:48:46 +0100

    need to finish off the checks

--- src/extension/jose.c
+++ src/extension/jose.c
@@ -33,38 +33,38 @@ void printLine(char *filepath, int n){
 }
 
 void printReg(struct Processor *proc , char **tokens) {
+  int start = 0 ;
+  int end = NUMBER_OF_REGISTERS;
+  if(!checkRegCommandIsValid(tokens+1)){
+    printInvalidCommandMessage();
+    return;
+  }
   if (strcmp("-r",tokens[0])==0) {
-    if((checkAllRegistersAreValid(char tokens++))){
-      int end = getRegisterNumber(tokens[2]);
-      for (int i=start; i<end/4 ; i++) {
-        for (int j=0;j<4 ; j++) {
-          printf("$%s = %i \t",(i*j) + j,getRegisterValue(proc,(i*j) + j));
-        }
-      printf("      \n");
-      }
-   }else{printInvalidCommandMessage()};
-  } else if (strcmp("-a",tokens[0])==0) {//not sure about i and j....    
-        for (int i=0; i<NUMBER_OF_REGISTERS/4 ; i++) {     
-          for (int j=0;j<4 ; j++) {
-           printf("%s = %i \t",tokens[(i*j) + j],getRegisterValue(proc,getRegisterNumber(tokens[(i*j) + j])));
-          }
-        printf("      \n");
-        }
-       
-  } else {
-    int i =1;
+    start = getRegisterNumber(tokens[1]);
+    end   = getRegisterNumber(tokens[2]);
+  }
+  else if (strcmp("-m",tokens[0])==0) {
+    tokens++;
+    int i =0;
     int j=0;
     while(tokens[i]!=NULL) {
-      for(int i = 0; i < 4; i++) {
-    for(int j = 0; j < 8; j++) {//check if registers are valid!!!
-       int reg = i * 8 + j;
-      printf("%s = %i \t",tokens[i],getRegisterValue(proc, getRegisterNumber(tokens[i])));
-      i++;
-      j++;
-      if (j==4) printf("      \n");
+        printf("%s = %i \t",tokens[i],getRegisterValue(proc, getRegisterNumber(tokens[i])));
+        i++;
+      if (i%8==0) printf("      \n");
     }
     printf("\n (JVG)");    
-  }
+  
+  } 
+  else if (!strcmp("-a",tokens[0])==0){
+    printInvalidCommandMessage();
+    return;
+  }       
+  for (int i=start; i<end/8 ; i++) {     
+    for (int j=0;j<8 ; j++) {
+      printf("%s = %i \t",tokens[(i*j) + j],getRegisterValue(proc,getRegisterNumber(tokens[(i*j) + j])));
+    }
+    printf("      \n");
+  }  
 }
 
 
